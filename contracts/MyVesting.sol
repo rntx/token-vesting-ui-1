364c51e31f85cd00c417f262c4f795957236b966
Mo Alraddadi <mp@pingtank.com>
2018-04-01 10:38:40 -0700

    Updated the sample contracts

--- contracts/MyVesting.sol
+++ contracts/MyVesting.sol
@@ -1,4 +1,4 @@
 pragma solidity ^0.4.11;
 
-import 'zeppelin-solidity/contracts/token/TokenVesting.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/TokenVesting.sol';
 import 'zeppelin-solidity/contracts/examples/SimpleToken.sol';
